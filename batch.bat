1695075c4b7ae78737b1c2d3bb539a3041e66479
abdalla adel essa <abdalla.essa@innovation-waves.com>
2014-12-11 17:36:24 +0200

    Second Commit

--- batch.bat
+++ batch.bat
@@ -2,15 +2,15 @@
 
 echo Deleting old file
 
-%ANDROID%\platform-tools\adb.exe shell rm -r /data/local/tmp/InjectionService.apk
+%ANDROID%\platform-tools\adb.exe shell rm -r /data/local/tmp/server.dex
 
 echo Pushing new file
-%ANDROID%\platform-tools\adb.exe push InjectionService.apk /data/local/tmp/
+%ANDROID%\platform-tools\adb.exe push server.dex /data/local/tmp/
 
 echo Running the class in the apk
 %ANDROID%\platform-tools\adb.exe shell export ANDROID=/data/local/tmp
 %ANDROID%\platform-tools\adb.exe shell cd /data/local/tmp/
-%ANDROID%\platform-tools\adb.exe shell dalvikvm -cp InjectionService.apk com.example.injectionservice.InjectionService
+%ANDROID%\platform-tools\adb.exe shell dalvikvm -cp server.dex Hello
 
 
 echo finished
