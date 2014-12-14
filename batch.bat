02a432438f0fe35cee5dcf956affc8423f756c67
abdalla adel essa <abdalla.essa@innovation-waves.com>
2014-12-14 09:53:21 +0200

    third commit

--- batch.bat
+++ batch.bat
@@ -3,16 +3,13 @@
 echo Deleting old file
 
 %ANDROID%\platform-tools\adb.exe shell rm -r /data/local/tmp/server.dex
+%ANDROID%\platform-tools\adb.exe shell rm -r /data/local/tmp/dalvik-cache
 
 echo Pushing new file
-%ANDROID%\platform-tools\adb.exe push server.dex /data/local/tmp/
+%ANDROID%\platform-tools\adb.exe push server.dex /data/local/tmp
 
 echo Running the class in the apk
-%ANDROID%\platform-tools\adb.exe shell export ANDROID=/data/local/tmp
-%ANDROID%\platform-tools\adb.exe shell cd /data/local/tmp/
-%ANDROID%\platform-tools\adb.exe shell dalvikvm -cp server.dex Hello
-
-
-echo finished
+%ANDROID%\platform-tools\adb.exe shell mkdir /data/local/tmp/dalvik-cache
+%ANDROID%\platform-tools\adb.exe shell ANDROID_DATA=/data/local/tmp dalvikvm -cp /data/local/tmp/server.dex InnowavesServer
 
 Pause:
