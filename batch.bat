a96522de1304f26b7b499527bfe9a197dade0fd3
abdalla adel essa <abdalla.essa@innovation-waves.com>
2014-12-14 10:19:40 +0200

    working version

--- batch.bat
+++ batch.bat
@@ -2,14 +2,14 @@
 
 echo Deleting old file
 
-%ANDROID%\platform-tools\adb.exe shell rm -r /data/local/tmp/server.dex
+%ANDROID%\platform-tools\adb.exe shell rm -r /data/local/tmp/InjectionService.apk
 %ANDROID%\platform-tools\adb.exe shell rm -r /data/local/tmp/dalvik-cache
 
 echo Pushing new file
-%ANDROID%\platform-tools\adb.exe push server.dex /data/local/tmp
+%ANDROID%\platform-tools\adb.exe push InjectionService.apk /data/local/tmp
 
 echo Running the class in the apk
 %ANDROID%\platform-tools\adb.exe shell mkdir /data/local/tmp/dalvik-cache
-%ANDROID%\platform-tools\adb.exe shell ANDROID_DATA=/data/local/tmp dalvikvm -cp /data/local/tmp/server.dex InnowavesServer
+%ANDROID%\platform-tools\adb.exe shell ANDROID_DATA=/data/local/tmp dalvikvm -cp /data/local/tmp/InjectionService.apk com.example.injectionservice.InnowavesServer
 
 Pause:
